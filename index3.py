ecda4e3960f9e6b500d2fe1d6345e32c409f0cc5
posture925 <feather0925@gmail.com>
2023-02-12 02:39:49 +0800

    reanme

--- index3.py
+++ index3.py
@@ -2,22 +2,20 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+#from api.chatgpt import ChatGPT
 import chatgpt
 
 import os
-
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
+working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 app = Flask(__name__)
 chatgpt = chatgpt()
-
 # domain root
 @app.route('/')
 def home():
     return 'Hello, World!'
-
 @app.route("/webhook", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -31,36 +29,19 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
-
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global working_status
+    
     if event.message.type != "text":
         return
-
-    if event.message.text == "說話":
-        working_status = True
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
-        return
-
-    if event.message.text == "閉嘴":
-        working_status = False
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="好的，我乖乖閉嘴 > <，如果想要我繼續說話，請跟我說 「說話」 > <"))
-        return
-
+    working_status = True
     if working_status:
-        chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
+        chatgpt.add_msg(f"Human:{event.message.text}?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
         chatgpt.add_msg(f"AI:{reply_msg}\n")
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
-
-
 if __name__ == "__main__":
     app.run()
